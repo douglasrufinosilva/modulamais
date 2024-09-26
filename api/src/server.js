c8bca630651ce29e4bc98da401fa56d6f1692d87
Douglas Rufino <douglasrufinosilva@gmail.com>
2024-09-25 23:09:24 -0300

    conexão com banco de dados

--- api/src/server.js
+++ api/src/server.js
@@ -1,9 +1,25 @@
 import express from 'express'
 import 'dotenv/config'
+import mysql from 'mysql'
 import { router as routes } from './routes.js'
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+const conectiondB = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: process.env.DB_PASSWORD,
+  database: 'registro-capivaras'
+})
+
+conectiondB.connect((error) => {
+  if(error) {
+    console.error("Erro na conexão.")
+  }
+
+  console.log("Banco de dados conectado.")
+})
 
 app.use(express.json())
 app.use(routes)
@@ -12,4 +28,4 @@ app.listen(PORT, () => {
   console.log("Server online!")
 })
 
-export { app }
+export { app, conectiondB }
